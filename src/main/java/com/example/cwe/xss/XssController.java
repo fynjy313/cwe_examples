1a68ab4eba1302e0b291e22ac21c52865a28accf
Barkhatov Anton <69BarkhatovAN@cbr.ru>
2024-10-02 16:39:35 +0700

    Попытка добавить Thymeleaf

--- src/main/java/com/example/cwe/xss/XssController.java
+++ src/main/java/com/example/cwe/xss/XssController.java
@@ -1,19 +1,24 @@
 package com.example.cwe.xss;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@RequestMapping("xss")
+@RequestMapping("/xss")
 public class XssController {
+
+    //TODO: beautify - JS ?
+
     private List<String> words = new ArrayList<>(List.of("Recorded words: "));
 
-    @GetMapping("/")
-    public String storedXssExample1() {
-        return words.toString();
-    }
+//    @GetMapping("/")
+//    public String storedXssExample1() {
+//        return words.toString();
+//    }
 
     @GetMapping("/exp")
     public String simpleXxsExample() {
@@ -27,9 +32,19 @@ public class XssController {
      * @param name Name to greeting
      * @return 'Hello' + name
      */
-    @GetMapping("reflected1")
+    @GetMapping("/reflected1")
     public String reflectedXssExample1(@RequestParam String name) {
         words.add(name);
         return "Hello, " + name + "!";
+    }
+
+    @GetMapping("/html")
+    public String jspExp() {
+        return "xss_page";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
     }
 }
